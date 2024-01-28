30ba8450a3ef3531df442c837f59d1333976813f
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-28 12:42:50 -0500

    App initialization

---
+++ Threads/Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Jeffrey Sweeney on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
