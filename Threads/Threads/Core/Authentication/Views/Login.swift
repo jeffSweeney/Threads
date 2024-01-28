8ae9b77a2d217089d1c96ad93e98764e08e8e0ff
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-28 14:46:21 -0500

    Define app structure and initial login file

---
+++ Threads/Threads/Core/Authentication/Views/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Threads
+//
+//  Created by Jeffrey Sweeney on 1/28/24.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
