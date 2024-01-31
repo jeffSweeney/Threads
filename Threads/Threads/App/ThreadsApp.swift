2b6d93f210af5e53e38ab46790849b941cbee36a
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-01-31 18:52:30 -0500

    Set up firebase in the app

--- Threads/Threads/App/ThreadsApp.swift
+++ Threads/Threads/App/ThreadsApp.swift
@@ -5,10 +5,22 @@
 //  Created by Jeffrey Sweeney on 1/28/24.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ThreadsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
